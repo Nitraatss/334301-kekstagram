421bc7d6a91145e70f8180a7c25de4267feca6d0
Vasilii <nitraatss@gmail.com>
2017-12-06 21:00:11 +0300

    Личный проект: максимум подвижности
    Добавлено регулирование эффектов с помощью перемещения ползунка при предпросмотре загруженного фото. Настройки эффектов отображаются при выбранных эффектах. При переключении между эффектами настройки сбрасываются.

--- js/form.js
+++ js/form.js
@@ -9,6 +9,24 @@
   // включение отображения формы загрузки
   var showUploadOverlay = function () {
     uploadOverlay.classList.remove('hidden');
+    effectPreview.style.transform = ('scale(' + (55 / 100) + ')');
+    effectsConfigHide();
+  };
+
+  // отключение отображения настроек эффектов
+  var effectsConfigHide = function () {
+    effectsLevel.classList.add('hidden');
+    effectPin.classList.add('hidden');
+    effectValLine.classList.add('hidden');
+    effectValue.classList.add('hidden');
+  };
+
+  // включение отображения настроек эффектов
+  var effectsConfigShow = function () {
+    effectsLevel.classList.remove('hidden');
+    effectPin.classList.remove('hidden');
+    effectValLine.classList.remove('hidden');
+    effectValue.classList.remove('hidden');
   };
 
   // увеличение масштаба изображения в форме кадрирования
@@ -80,6 +98,52 @@
     }
   };
 
+  // создание DOM элемента для изображения с эффектом
+  var effectsDOMelements = function () {
+    if (effectName === 'effect-chrome') {
+      afterEffectImage = window.data.creatDOMElement(uploadEffect, '.effect-chrome');
+    }
+
+    if (effectName === 'effect-sepia') {
+      afterEffectImage = window.data.creatDOMElement(uploadEffect, '.effect-sepia');
+    }
+
+    if (effectName === 'effect-marvin') {
+      afterEffectImage = window.data.creatDOMElement(uploadEffect, '.effect-marvin');
+    }
+
+    if (effectName === 'effect-phobos') {
+      afterEffectImage = window.data.creatDOMElement(uploadEffect, '.effect-phobos');
+    }
+
+    if (effectName === 'effect-heat') {
+      afterEffectImage = window.data.creatDOMElement(uploadEffect, '.effect-heat');
+    }
+  };
+
+  // изменения значений эффектов от ползунка
+  var changeEffect = function (effectVal) {
+    if (effectName === 'effect-chrome') {
+      afterEffectImage.style.filter = 'grayscale(' + (effectVal / 100) + ')';
+    }
+
+    if (effectName === 'effect-sepia') {
+      afterEffectImage.style.filter = 'sepia(' + (effectVal / 100) + ')';
+    }
+
+    if (effectName === 'effect-marvin') {
+      afterEffectImage.style.filter = 'invert(' + effectVal + '%)';
+    }
+
+    if (effectName === 'effect-phobos') {
+      afterEffectImage.style.filter = 'blur(' + ((effectVal * 3) / 100) + 'px)';
+    }
+
+    if (effectName === 'effect-heat') {
+      afterEffectImage.style.filter = 'brightness(' + ((effectVal * 3) / 100) + ')';
+    }
+  };
+
   var formUploadSelectImage = window.data.creatDOMElement(document, '#upload-select-image');
   var fieldUploadFile = window.data.creatDOMElement(formUploadSelectImage, '#upload-file');
   var uploadOverlay = window.data.creatDOMElement(formUploadSelectImage, '.upload-overlay');
@@ -94,108 +158,14 @@
   var uploadHashtags = window.data.creatDOMElement(uploadOverlay, '.upload-form-hashtags');
   var effectControls = window.data.creatDOMElement(uploadOverlay, '.upload-effect-controls');
   var effectsLevel = window.data.creatDOMElement(effectControls, '.upload-effect-level');
-  
   var effectValue = window.data.creatDOMElement(effectControls, '.upload-effect-level-value');
   var effectPin = window.data.creatDOMElement(effectControls, '.upload-effect-level-pin');
   var effectValLine = window.data.creatDOMElement(effectControls, '.upload-effect-level-val');
- 
 
   var maxX;
   var effectName;
-  effectsLevel.classList.add('hidden');
-  effectPin.classList.add('hidden');
-  effectValLine.classList.add('hidden');
-  effectValue.classList.add('hidden');
   var afterEffectImage;
 
-  
-  
-
-  effectPin.addEventListener('mousedown', function (dEvent) {
-    dEvent.preventDefault();
-    console.log(effectName);
-
-    // задаем стартовые координаты
-    var startCoords = {
-      X: dEvent.clientX
-    };
-
-    // перемещаем  пин
-    var onMouseMove = function (mEvent) {
-      mEvent.preventDefault();
-
-      // расчет смещения
-      var move = {
-        moveX: startCoords.X - mEvent.clientX,
-      };
-
-      // переопределение стартовой позиции
-      startCoords = {
-        X: mEvent.clientX,
-      };
-
-      // смена позиции по мере движения
-      if ((effectPin.offsetLeft - move.moveX) < maxX)
-      
-      {
-        if ((effectPin.offsetLeft - move.moveX) > 0){
-        effectPin.style.left = ((effectPin.offsetLeft - move.moveX)*100)/maxX + '%';
-          effectValue.value = Math.floor(((effectPin.offsetLeft - move.moveX) * 100) / maxX) ;
-          effectValLine.style.width = ((effectPin.offsetLeft - move.moveX) * 100) / maxX + '%';
-          
-          
-
-
-         
-          
-        
-        }
-        else {
-          effectPin.style.left = '0%';
-          effectValLine.style.width = '0%';
-          effectValue.value=0;
-        }
-      }
-      else {
-        effectPin.style.left = '100%';
-        effectValue.value = 100;
-        effectValLine.style.width = '100%';
-      }
-
-      if (effectName === 'effect-chrome') {
-        afterEffectImage.style.filter = 'grayscale(' + (effectValue.value / 100) + ')';
-      }
-
-      if (effectName === 'effect-sepia') {
-        afterEffectImage.style.filter = 'sepia(' + (effectValue.value / 100) + ')';
-      }
-
-      if (effectName === 'effect-marvin') {
-        afterEffectImage.style.filter = 'invert(' + (effectValue.value) + '%)';
-      }
-
-      if (effectName === 'effect-phobos') {
-        afterEffectImage.style.filter = 'blur(' + ((effectValue.value * 3) / 100) + 'px)';
-      }
-
-      if (effectName === 'effect-heat') {
-        afterEffectImage.style.filter = 'brightness(' + ((effectValue.value * 3) / 100) + ')';
-      }
-    };
-
-    var onMouseUp = function (uEvent) {
-      uEvent.preventDefault();
-      console.log(startCoords.X);
-
-      document.removeEventListener('mousemove', onMouseMove);
-      document.removeEventListener('mouseup', onMouseUp);
-    };
-
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
-  }
-  );
-  
   // отключение полноэкранного формы загрузки по нажатию Esc
   document.addEventListener('keydown', function (event) {
     if (event.keyCode === window.data.ESC_KEYCODE && event.target !== uploadDescription) {
@@ -222,74 +192,87 @@
   // уменьшение масштаба фото
   resizeButtonDec.addEventListener('click', onResizeButtonDecClick);
 
+  effectPin.addEventListener('mousedown', function (dEvent) {
+    dEvent.preventDefault();
+
+    // задаем стартовые координаты
+    var startCoords = {
+      X: dEvent.clientX
+    };
+
+    // перемещение ползунка
+    var onMouseMove = function (mEvent) {
+      mEvent.preventDefault();
+
+      // расчет смещения
+      var move = {
+        moveX: startCoords.X - mEvent.clientX,
+      };
+
+      // переопределение стартовой позиции
+      startCoords = {
+        X: mEvent.clientX,
+      };
+
+      // смена позиции по мере движения
+      // изменение желтой полоски и инпута с цифрами
+      if ((effectPin.offsetLeft - move.moveX) < maxX) {
+        if ((effectPin.offsetLeft - move.moveX) > 0) {
+          effectPin.style.left = ((effectPin.offsetLeft - move.moveX) * 100) / maxX + '%';
+          effectValue.value = Math.floor(((effectPin.offsetLeft - move.moveX) * 100) / maxX);
+          effectValLine.style.width = ((effectPin.offsetLeft - move.moveX) * 100) / maxX + '%';
+        } else {
+          effectPin.style.left = '0%';
+          effectValLine.style.width = '0%';
+          effectValue.value = 0;
+        }
+      } else {
+        effectPin.style.left = '100%';
+        effectValue.value = 100;
+        effectValLine.style.width = '100%';
+      }
+
+      // изменение значений эффектов
+      changeEffect(effectValue.value);
+    };
+
+    var onMouseUp = function (uEvent) {
+      uEvent.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  }
+  );
+
   // наложение эффектов на фото
   uploadEffect.addEventListener('click', function (event) {
-    
     if (event.target.type === 'radio') {
       effectPreview.classList = 'upload-form-preview';
       effectName = event.target.id.substring(7);
       effectPreview.classList.add(effectName);
 
-      if (effectName === 'effect-chrome') {
-        afterEffectImage = window.data.creatDOMElement(uploadEffect, '.effect-chrome');
-      }
+      effectsDOMelements();
 
-      if (effectName === 'effect-sepia') {
-        afterEffectImage = window.data.creatDOMElement(uploadEffect, '.effect-sepia');
-      }
+      if (effectName !== 'effect-none') {
+        effectsConfigShow();
 
-      if (effectName === 'effect-marvin') {
-        afterEffectImage = window.data.creatDOMElement(uploadEffect, '.effect-marvin');
-      }
-
-      if (effectName === 'effect-phobos') {
-        afterEffectImage = window.data.creatDOMElement(uploadEffect, '.effect-phobos');
-      }
-
-      if (effectName === 'effect-heat') {
-        afterEffectImage = window.data.creatDOMElement(uploadEffect, '.effect-heat');
-      }
-
-
-      if (effectName !== 'effect-none'){
-      effectPin.classList.remove('hidden');
-      effectValLine.classList.remove('hidden');
-      effectValue.classList.remove('hidden');
-        effectsLevel.classList.remove('hidden');
         effectPin.style.left = '20%';
         effectValLine.style.width = '20%';
         effectValue.value = '20';
-      maxX = effectPin.offsetLeft * 5;
+        maxX = effectPin.offsetLeft * 5;
 
-        if (effectName === 'effect-chrome') {
-          afterEffectImage.style.filter = 'grayscale(' + (effectValue.value / 100) + ')';
-        }
+        changeEffect(effectValue.value);
+      } else {
+        effectsConfigHide();
 
-        if (effectName === 'effect-sepia') {
-          afterEffectImage.style.filter = 'sepia(' + (effectValue.value / 100) + ')';
-        }
-
-        if (effectName === 'effect-marvin') {
-          afterEffectImage.style.filter = 'invert(' + (effectValue.value) + '%)';
-        }
-
-        if (effectName === 'effect-phobos') {
-          afterEffectImage.style.filter = 'blur(' + ((effectValue.value * 3) / 100) + 'px)';
-        }
-
-        if (effectName === 'effect-heat') {
-          afterEffectImage.style.filter = 'brightness(' + ((effectValue.value * 3) / 100) + ')';
-        }
-    }
-    else {
-        effectPin.classList.add('hidden');
-        effectValLine.classList.add('hidden');
-        effectValue.classList.add('hidden');
-        effectsLevel.classList.add('hidden');
         if (effectName === 'effect-none') {
           afterEffectImage.style.filter = null;
         }
-        }
+      }
     }
   }
   );
